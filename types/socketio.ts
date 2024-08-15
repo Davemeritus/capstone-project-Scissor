363a6a377b6c83a766d4f7f234782295c7b8fa26
David Okunade <davidokunade1@gmail.com>
2024-08-15 05:39:39 +0100

    debugging socket io palava

--- types/socketio.ts
+++ types/socketio.ts
@@ -1,3 +1,15 @@
+// import { Server as ServerIO } from "socket.io";
+// import { Server as NetServer, Socket } from "net";
+// import { NextApiResponse } from "next";
+
+// export type NextApiResponseServerIo = NextApiResponse & {
+//   socket: Socket & {
+//     server: NetServer & {
+//       io: ServerIO;
+//     };
+//   };
+// };
+
 import { Server as ServerIO } from "socket.io";
 import { Server as NetServer, Socket } from "net";
 import { NextApiResponse } from "next";
@@ -5,7 +17,7 @@ import { NextApiResponse } from "next";
 export type NextApiResponseServerIo = NextApiResponse & {
   socket: Socket & {
     server: NetServer & {
-      io: ServerIO;
+      io?: ServerIO; // io is now optional to handle scenarios where it's not initialized
     };
   };
 };
