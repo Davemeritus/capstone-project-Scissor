f3aa3a54f6c39525ab67c2386a9ec83bdd9f2c1f
David Okunade <davidokunade1@gmail.com>
2024-08-15 03:53:05 +0100

    add Netlify toml file

--- next.config.mjs
+++ next.config.mjs
@@ -1,27 +1,41 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     async headers() {
+//       return [
+//         {
+//           source: '/(.*)',
+//           headers: [
+//             {
+//               key: 'Host',
+//               value: 'scissors.talut.tech',
+//             },
+//           ],
+//         },
+//       ];
+//     },
+//     // Add this to ensure NextAuth.js can handle the auth routes correctly
+//     async rewrites() {
+//       return [
+//         {
+//           source: '/api/auth/:path*',
+//           destination: '/api/auth/:path*',
+//         },
+//       ];
+//     },
+//   };
+  
+//   export default nextConfig;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async headers() {
-      return [
-        {
-          source: '/(.*)',
-          headers: [
-            {
-              key: 'Host',
-              value: 'scissors.talut.tech',
-            },
-          ],
-        },
-      ];
-    },
-    // Add this to ensure NextAuth.js can handle the auth routes correctly
-    async rewrites() {
-      return [
-        {
-          source: '/api/auth/:path*',
-          destination: '/api/auth/:path*',
-        },
-      ];
-    },
-  };
-  
-  export default nextConfig;
+  async rewrites() {
+    return [
+      {
+        source: '/api/auth/:path*',
+        destination: '/api/auth/:path*', // This rewrite can be retained if needed for auth routes
+      },
+    ];
+  },
+};
+
+export default nextConfig;
