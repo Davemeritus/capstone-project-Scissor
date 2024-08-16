e4f2b5ac612dbbd1bc76aabd6cac8334fdadb55e
David Okunade <davidokunade1@gmail.com>
2024-08-16 13:30:06 +0100

    updating yaml file

--- actions/logout.ts
+++ actions/logout.ts
@@ -1,12 +1,3 @@
-// import { signOut } from "next-auth/react";
-// import { loginRoute } from "@/routes";
-
-// export const logout = async () => {
-//   await signOut({
-//     callbackUrl: loginRoute,
-//   });
-// };
-
 
 import { signOut } from "next-auth/react";
 import { loginRoute } from "@/routes";
