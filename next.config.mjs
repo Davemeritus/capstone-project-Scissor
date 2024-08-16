706ed7eb25132b59880fb8478c7b17757a13b538
David Okunade <davidokunade1@gmail.com>
2024-08-16 09:57:44 +0100

    davemeritus-tech

--- next.config.mjs
+++ next.config.mjs
@@ -42,13 +42,45 @@
 
 
 
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   async rewrites() {
+//     return [
+//       {
+//         source: "/api/auth/:path*",
+//         destination: "/api/auth/:path*",
+//       },
+//     ];
+//   },
+//   experimental: {
+//     serverComponentsExternalPackages: ["pino", "pino-pretty"],
+//   },
+// };
+
+// export default nextConfig;
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async headers() {
+    return [
+      {
+        // Apply headers to all routes
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Host',
+            value: 'davemeritus.tech',
+          },
+        ],
+      },
+    ];
+  },
   async rewrites() {
     return [
       {
-        source: "/api/auth/:path*",
-        destination: "/api/auth/:path*",
+        source: '/api/auth/:path*',
+        destination: '/api/auth/:path*',
       },
     ];
   },
@@ -58,32 +90,3 @@ const nextConfig = {
 };
 
 export default nextConfig;
-
-
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   async headers() {
-//     return [
-//       {
-//         source: '/(.*)',
-//         headers: [
-//           {
-//             key: 'Host',
-//             value: 'scissor.talut.tech',
-//           },
-//         ],
-//       },
-//     ];
-//   },
-//   // Add this to ensure NextAuth.js can handle the auth routes correctly
-//   async rewrites() {
-//     return [
-//       {
-//         source: '/api/auth/:path*',
-//         destination: '/api/auth/:path*',
-//       },
-//     ];
-//   },
-// };
-
-// export default nextConfig;
